022b3fb4c67771bb447a921e01f7e9cb7e40066b
pedram <pedram.s218@gmail.com>
2023-05-06 13:24:49 +0430

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com"],
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [320, 420, 768, 1024, 1200],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
